d9997a1c5c0a17a7d7d885f4a4171db9558b4b8c
Danny <baquedanny9@gmail.com>
2022-05-31 01:01:26 -0500

    day2

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,10 @@
+import { Template } from "@angular/compiler/src/render3/r3_ast";
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-server',
+    templateUrl: ''
+})
+export class ServerComponent {
+
+}
