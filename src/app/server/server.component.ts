a7fea1bc8339453f4180bb018c988588f1d3bb8d
Danny <baquedanny9@gmail.com>
2022-05-31 02:51:57 -0500

    ok

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-server',
-    templateUrl: ''
+    templateUrl: './server.component.html'
 })
 export class ServerComponent {
 
