74c556ce6b9fa53fff912dce2c33abf1d30e7507
Danny <baquedanny9@gmail.com>
2022-07-03 13:54:20 -0500

    Creacion de componentes con cli ng g component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PrimerComponenteComponent } from './primer-componente/primer-componente.component';
+import { SegundoComponenteComponent } from './segundo-componente/segundo-componente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrimerComponenteComponent,
+    SegundoComponenteComponent
   ],
   imports: [
     BrowserModule
